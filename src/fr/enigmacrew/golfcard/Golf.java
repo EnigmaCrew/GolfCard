3c05a4e37528400c69167ca703b08b60f7f95575
CS-Sauvaj <hugosimonyjungo@aol.fr>
2021-02-14 13:07:28 +0000

    Setup the skelleton project

---
+++ src/fr/enigmacrew/golfcard/Golf.java
@@ -0,0 +1,8 @@
+package fr.enigmacrew.golfcard;
+
+import javax.swing.JFrame;
+
+public class Golf extends JFrame {
+	private static final long serialVersionUID = 1L;
+
+}
--- src/fr/enigmacrew/golfcard/Main.java
+++ src/fr/enigmacrew/golfcard/Main.java
@@ -1,8 +1,22 @@
 package fr.enigmacrew.golfcard;
 
 public class Main {
+	
+	/*
+	 *  @authors
+	 *   - Hugo Simony-Jungo
+	 *   - Celian Raimbault
+	 */
 
 	public static void main(String[] args) {
+		
+		/*
+		 * Start the golf game
+		 */
+		
+		// Start the main frame
+		Golf golf = new Golf();
+		golf.setVisible(true);
 		
 	}
 	
