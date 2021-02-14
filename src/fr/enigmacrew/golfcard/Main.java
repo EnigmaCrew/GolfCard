68a6d9149284e40d192e3dc854ec06ba7b0ad9fc
Celian RAIMBAULT <celian.raimbault@epita.fr>
2021-02-14 17:50:52 +0000

    Updated main

--- src/fr/enigmacrew/golfcard/Main.java
+++ src/fr/enigmacrew/golfcard/Main.java
@@ -13,21 +13,8 @@ public class Main {
 		/*
 		 * Start the golf game
 		 */
-        Game game = new Game(6, true);
-		game.debug();
-		game.step(new GameAction(GameAction.Kind.TURN, 1));
-		game.debug();
-		game.step(new GameAction(GameAction.Kind.DRAW, 2));
-		game.debug();
-		game.step(new GameAction(GameAction.Kind.DRAW, -1));
-		game.debug();
-
-		Integer[] scores = game.getScores();
-		System.out.println("Score P1 : " + scores[0].toString());
-		System.out.println("Score P2 : " + scores[1].toString());
-
 		// Start the main frame
-		// Golf golf = new Golf();
-		// golf.setVisible(true);
+		Golf golf = new Golf();
+		golf.setVisible(true);
 	}
 }
